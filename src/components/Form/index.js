616b6ae127854e80563af65ce7e0428652bed938
thiagohdaqwl <thiagohdaqw@gmail.com>
2021-03-17 19:59:21 -0300

    (#84) refatorando formulario

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,22 +1,41 @@
 import React from "react";
 import './index.css';
 
-export default function Form(props) {
 
+const Field = ({errorMsg, children}) => {
     return (
-        <div className="Form">
-            <div className="logo" />
-            <div className="Fields">
-                <h4>
-                    {props.title}
-                </h4>
-                <div>
-                    {props.fields}
-                </div>
-                <div>
-                    {props.buttons}
-                </div>
-            </div>
+        <div className="field">
+            {children}
+            <div className="field error">{errorMsg}</div>
         </div>
+    )
+}
+
+const Footer = ({children}) => {
+    return (
+        <div className="button">
+            {children}
+        </div>
+    )
+}
+
+function Form({title, children, onSubmit}) {
+    return (
+        <main>
+            <div className="logo" />
+            <form className="form" onSubmit={onSubmit}>
+                <h4>
+                    {title}
+                </h4>
+                <div className="fields">
+                    {children}
+                </div>
+            </form>
+        </main>
     );
 }
+
+
+Form.Field = Field;
+Form.Footer = Footer;
+export default Form;
--- src/pages/RegisterStudent/index.js
+++ src/pages/RegisterStudent/index.js
@@ -23,11 +23,39 @@ import Button from '../../components/Button';
 export default function RegisterStudent() {
   const [courses, setCourses] = React.useState([])
 
-  const { register, handleSubmit } = useForm({
+  const { register, handleSubmit, errors } = useForm({
     resolver: yupResolver(schema),
   });
   
-  const onSubmit = data => console.log(data);
+  console.log(errors)
+  const onSubmit = data => {
+    console.log(data)
+    return;
+
+    const body = {
+      "reg_student": 19212244,
+      "name": "Thiago Paiva",
+      "id_course": 15,
+      "email": "thiago.smaaa1111paa@aao.unb.br",
+      "password": "password"
+    }
+
+    fetch('http://localhost:5000/anunbis/api/student', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body)
+    }).then(response => response)
+    .then(rs => {
+      console.log(rs)
+      console.log(rs.json())
+      if(rs.ok){
+        console.log("deu certo")
+      }
+      if(rs.status === 409){
+        console.log("estudante ja cadastrado")
+      }
+    })
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -39,28 +67,21 @@ export default function RegisterStudent() {
     fetchData();
   }, [])
 
-
-  const fields = (
-    <form className="Fields">
-      <Input type="text" text="Nome" name="name" register={register} />
-      <Select id="courses" options={courses} name="id_course" register={register} />
-      <Input type="text" text="Matrícula" name="reg_student" register={register} />
-      <Input type="email" text="Email Institucional" name="email" register={register} />
-      <Input type="password" text="Senha" name="password" register={register} />
-      <Input type="password" text="Confirmar Senha" name="co_password" register={register} />
-    </form>
-  );
-  const buttons = (
-    <div className="Button">
-      <Button text="CANCELAR" />
-      <Button text="CONFIRMAR" onClick={handleSubmit(onSubmit)}/>
-    </div>
-  );
-
   return (
     <div className="RegisterStudent">
       <header className="Header">
-        <Form title="Cadastro de Aluno" fields={fields} buttons={buttons} />
+        <Form title="Cadastro de Aluno" onSubmit={handleSubmit(onSubmit)}>
+          <Form.Field errorMsg={errors.name?.message}><Input type="text" text="Nome" name="name" register={register} /></Form.Field>
+          <Form.Field errorMsg={errors.id_course?.message}><Select id="courses" options={courses} name="id_course" register={register} /></Form.Field>
+          <Form.Field errorMsg={errors.reg_student?.message}><Input type="text" text="Matrícula" name="reg_student" register={register} /></Form.Field>
+          <Form.Field errorMsg={errors.email?.message}><Input type="email" text="Email Institucional" name="email" register={register} /></Form.Field>
+          <Form.Field errorMsg={errors.password?.message}><Input type="password" text="Senha" name="password" register={register} /></Form.Field>
+          <Form.Field errorMsg={errors.co_password?.message}><Input type="password" text="Confirmar Senha" name="co_password" register={register} /></Form.Field>
+          <Form.Footer>
+            <Button text="CANCELAR" />
+            <Button text="CONFIRMAR" type="submit"/>
+          </Form.Footer>
+        </Form>
       </header>
     </div>
   )
