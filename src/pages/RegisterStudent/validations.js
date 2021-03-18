386ee1785fe58a0670b52e165cf057e5dd85cff7
RcleydsonR <rafael.cleydson@gmail.com>
2021-03-18 11:43:31 -0300

    (#84) Adicionando validacao de caracteres maximos
    Co-authored-by: Eduardo Afonso <oeduardoafonso@gmail.com>

--- src/pages/RegisterStudent/validations.js
+++ src/pages/RegisterStudent/validations.js
@@ -3,7 +3,9 @@ import * as yup from 'yup';
 const schema = yup.object().shape({
     name: yup
     .string()
+    .trim()
     .min(2, "Nome deve conter pelo menos dois caracteres.")
+    .max(254, "Nome deve ter no máximo 254 caracteres.")
     .required("Nome deve ser preenchido."),
 
     id_course: yup
@@ -12,18 +14,23 @@ const schema = yup.object().shape({
 
     reg_student: yup
     .string()
+    .trim()
     .length(9, "Matrícula deve conter exatamente 9 dígitos.")
     .matches(/^[0-9]*$/, "Matricula deve conter somente números.")
     .required("Matricula deve ser preenchida."),
 
     email: yup
     .string()
+    .trim()
+    .lowercase()
     .matches(/^[0-9]+@aluno.unb.br$/, "Formato do email deve ser: \"matricula@aluno.unb.br\"")
+    .max(90, "O email deve ter tamanho máximo de 90 caracteres.")
     .required("Email deve ser preenchido."),
 
     password: yup
     .string()
     .min(8, "Senha deve ter pelo menos 8 caracteres.")
+    .max(100, "Senha deve ter no máximo 100 caracteres.")
     .required("Senha deve ser preenchida."),
 
     co_password: yup
