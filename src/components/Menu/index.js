593483dd890e7da6f1e11491451041c07ed8ba2e
oEduardoAfonso <oeduardoafonso@gmail.com>
2021-03-22 20:32:58 -0300

    (#102) Criando campo de pesquisa que retorna JSON da API
    Co-authored-by: Victor Hugo <hugo.victorcsi@gmail.com>
    Co-authored-by: Thiago Paiva <thiagohdaqw@gmail.com>

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import './index.css'
+import Name_Logo from "../../assets/Name_Logo.png";
+import Input from '../Input';
 
-export default function Menu(){
+export default function Menu() {
+    const [teachers, setTeachers] = React.useState([{}])
+    const [searchTxt, setSearch] = React.useState("")
+
+    const onSubmit = (data) => {
+        if(data.key === 'Enter' && data.target.value.trim().length > 0)
+        setSearch(data.target.value.trim())}
+
+    React.useEffect(() => {
+        async function fetchData() {
+          const url = process.env.REACT_APP_API_HOST + "/professor/" + searchTxt;
+          const response = await fetch(url)
+          const data = await response.json()
+    
+          setTeachers(data);
+          
+        }
+        fetchData();
+      }, [searchTxt]);
+      console.log(teachers)
+
     return (
         <div className="Menu">
-
-        </div>
+            <div className="Logo">
+                <img src={Name_Logo} alt="logo"></img>
+            </div>
+                <Input type="text" placeholder="Informe o nome do professor" onkeydown={onSubmit}/>
+            </div>
     );
 }
