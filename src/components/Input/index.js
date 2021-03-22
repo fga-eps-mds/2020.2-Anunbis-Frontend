7c1b3629cae9efae830a270dda7acdcc40801ded
oEduardoAfonso <oeduardoafonso@gmail.com>
2021-03-22 20:32:58 -0300

    (#102) Criando campo de pesquisa que retorna JSON da API
    Co-authored-by: Victor Hugo <hugo.victorcsi@gmail.com>
    Co-authored-by: Thiago Paiva <thiagohdaqw@gmail.com>

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -5,7 +5,13 @@ export default function Input(props) {
 
     return (
         <div className="Input">
-            <input type={props.type} placeholder={props.text} value={props.value} name={props.name} ref={props.register} autoComplete="off" >
+            <input type={props.type} 
+            placeholder={props.text} 
+            value={props.value} 
+            name={props.name} 
+            ref={props.register} 
+            autoComplete="off" 
+            onKeyPress={props.onkeydown}>
             </input>
         </div>
     );
