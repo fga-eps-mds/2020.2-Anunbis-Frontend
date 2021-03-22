0336d746fc9094f30b9c125d987897b19b00a637
oEduardoAfonso <oeduardoafonso@gmail.com>
2021-03-22 18:05:54 -0300

    (#102) Criando componente de Menu e Colocando BG APP

--- src/pages/Application/index.js
+++ src/pages/Application/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Menu from '../../components/Menu';
+import './index.css'
 
 export default function Application (){
     return(
         <div className="Application">
-
+            <Menu />
+            <div className="Content">
+        
+            </div>
         </div>
     );
 }
