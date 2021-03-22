b63b8a06ce50447a88d95b566782b75e2e573bad
RcleydsonR <rafael.cleydson@gmail.com>
2021-03-21 23:46:00 -0300

    (#91) Adicionando validacao dos inputs

--- src/pages/RegisterProfessor/index.js
+++ src/pages/RegisterProfessor/index.js
@@ -5,20 +5,21 @@ import './index.css';
 import Form from '../../components/Form';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import schema from './validations';
 
 export default function RegisterProfessor(){
     const { register, handleSubmit, errors } = useForm({
-        resolver: yupResolver(),
+        resolver: yupResolver(schema),
       });
 
     return (
         <div className="RegisterProfessor">
             <header className="Header">
                 <Form title="Cadastro de Professor" onSubmit={handleSubmit()} link="ALUNO?" endereco={`/`}>
-                    <Form.Field><Input type="text" text="Nome" name="name" register={register} /></Form.Field>
-                    <Form.Field><Input type="email" text="Email Institucional" name="email" register={register} /></Form.Field>
-                    <Form.Field> <Input type="password" text="Senha" name="password" register={register} /></Form.Field>
-                    <Form.Field><Input type="password" text="Confirmar Senha" name="co_password" register={register} /></Form.Field>
+                    <Form.Field errorMsg={errors.name?.message}><Input type="text" text="Nome" name="name" register={register} /></Form.Field>
+                    <Form.Field errorMsg={errors.email?.message}><Input type="email" text="Email Institucional" name="email" register={register} /></Form.Field>
+                    <Form.Field errorMsg={errors.password?.message}> <Input type="password" text="Senha" name="password" register={register} /></Form.Field>
+                    <Form.Field errorMsg={errors.co_password?.message}><Input type="password" text="Confirmar Senha" name="co_password" register={register} /></Form.Field>
                     <Form.Footer>
                         <Button text="CANCELAR" />
                         <Button text="CONFIRMAR" type="submit"/>
---
+++ src/pages/RegisterProfessor/validations.js
@@ -0,0 +1,31 @@
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+    name: yup
+    .string()
+    .trim()
+    .min(2, "Nome deve conter pelo menos dois caracteres")
+    .max(254, "Nome deve ter no máximo 254 caracteres.")
+    .required("Nome deve ser preenchido."),
+
+    email: yup
+    .string()
+    .trim()
+    .lowercase()
+    .matches(/^[a-z . 0-9]+@unb.br$/, "Formato do email deve ser: \"matricula(nome)@unb.br\"")
+    .max(90, "O email deve ter tamanho máximo de 90 caracteres.")
+    .required("Email deve ser preenchido."),
+
+    password: yup
+    .string()
+    .min(8, "Senha deve ter pelo menos 8 caracteres.")
+    .max(100, "Senha deve ter no máximo 100 caracteres.")
+    .required("Senha deve ser preenchida."),
+
+    co_password: yup
+    .string()
+    .oneOf([yup.ref('password')], "Senhas devem ser iguais.")
+    .required("Confirmação da senha deve ser preenchida."),
+});
+
+export default schema;
