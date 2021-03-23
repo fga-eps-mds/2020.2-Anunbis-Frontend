98687d81b187975dc71b116c46593686b5cd29a8
RcleydsonR <rafael.cleydson@gmail.com>
2021-03-22 23:25:29 -0300

    (#91) Adicionando submissão dos dados cadastrais

--- src/pages/RegisterProfessor/index.js
+++ src/pages/RegisterProfessor/index.js
@@ -11,11 +11,20 @@ export default function RegisterProfessor(){
     const { register, handleSubmit, errors } = useForm({
         resolver: yupResolver(schema),
       });
+    
+    function onSubmit(data){
+        const body = {
+            name: data.name,
+            email: data.email,
+            password: data.password
+        }
+        console.log(body);
+    }
 
     return (
         <div className="RegisterProfessor">
             <header className="Header">
-                <Form title="Cadastro de Professor" onSubmit={handleSubmit()} link="ALUNO?" endereco={`/`}>
+                <Form title="Cadastro de Professor" onSubmit={handleSubmit(onSubmit)} link="ALUNO?" endereco={`/`}>
                     <Form.Field errorMsg={errors.name?.message}><Input type="text" text="Nome" name="name" register={register} /></Form.Field>
                     <Form.Field errorMsg={errors.email?.message}><Input type="email" text="Email Institucional" name="email" register={register} /></Form.Field>
                     <Form.Field errorMsg={errors.password?.message}> <Input type="password" text="Senha" name="password" register={register} /></Form.Field>
