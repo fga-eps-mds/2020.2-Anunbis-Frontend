e8d698a2b61d1b44f0e6d94221024611db383c4b
Roberto <robertosantana2001@gmail.com>
2021-03-23 00:23:12 -0300

    (#104) Configurando react hook e state na pagina de login de aluno
    Foi configurado as funçoes dessas bibliotecas. No hook, foi configurado as validacoes de email e senha. No state, foi configurado um componente de aparicao variavel

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,24 +1,33 @@
 import React from "react";
 import Button from "../../components/Button";
 import Form from "../../components/Form";
-import Input from "../../components/Input"
-import "./index.css"
+import Input from "../../components/Input";
+import "./index.css";
+import schema from "./validations"
 
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 
 export default function Login() {
-    const { register, handleSubmit, errors } = useForm([]);
+    const [erroLogin, setErroLogin] = React.useState(''); 
+    const { register, handleSubmit, errors } = useForm({
+        resolver: yupResolver(schema),
+    });
 
     function onSubmit(data) {
-            
+        const body = {
+            email: data.email,
+            password: data.password
+        }
+        console.log(data);
     }
     return (
         <div className="Login">
             <header className="Header">
                 <Form onSubmit={handleSubmit(onSubmit)}>
-                    <Form.Field errorMsg="Email errado"><Input type="text" text="Email Instuticional" name="email" register={register} /> </Form.Field>
-                    <Form.Field errorMsg="Senha Errada!"><Input type="password" text="Senha" name="password" register={register} /> </Form.Field>
+                    <Form.Field><div className="Erro">{erroLogin}</div></Form.Field>
+                    <Form.Field errorMsg={errors.email?.message}><Input type="text" text="Email Instuticional" name="email" register={register} /> </Form.Field>
+                    <Form.Field errorMsg={errors.password?.message}><Input type="password" text="Senha" name="password" register={register} /> </Form.Field>
                     <Form.Footer>
                         <Button text="CONFIRMAR" />
                     </Form.Footer>
