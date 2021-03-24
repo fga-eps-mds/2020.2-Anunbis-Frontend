207098cf74dd270bf9b235cc142aea3df5e70136
RcleydsonR <rafael.cleydson@gmail.com>
2021-03-24 00:49:10 -0300

    (#91) Atualizando validacao de matricula de professor

--- src/pages/RegisterProfessor/validations.js
+++ src/pages/RegisterProfessor/validations.js
@@ -4,34 +4,36 @@ const schema = yup.object().shape({
     name: yup
     .string()
     .trim()
+    .required("Nome deve ser preenchido.")
     .min(2, "Nome deve conter pelo menos dois caracteres")
-    .max(254, "Nome deve ter no máximo 254 caracteres.")
-    .required("Nome deve ser preenchido."),
+    .max(254, "Nome deve ter no máximo 254 caracteres."),
 
     reg_professor: yup
     .string()
     .trim()
-    .matches(/^[0-9]*$/, "Matricula deve conter somente números.")
-    .required("Matricula deve ser preenchida."),
+    .required("Matricula deve ser preenchida.")
+    .length(11, "Matrícula deve conter exatamente 11 dígitos.")
+    .matches(/^[0-9]*$/, "Matricula deve conter somente números."),
+    
 
     email: yup
     .string()
     .trim()
     .lowercase()
+    .required("Email deve ser preenchido.")
     .matches(/^[a-z . 0-9]+@unb.br$/, "Formato do email deve ser: \"matricula(nome)@unb.br\"")
-    .max(90, "O email deve ter tamanho máximo de 90 caracteres.")
-    .required("Email deve ser preenchido."),
+    .max(90, "O email deve ter tamanho máximo de 90 caracteres."),
 
     password: yup
     .string()
+    .required("Senha deve ser preenchida.")
     .min(8, "Senha deve ter pelo menos 8 caracteres.")
-    .max(100, "Senha deve ter no máximo 100 caracteres.")
-    .required("Senha deve ser preenchida."),
+    .max(100, "Senha deve ter no máximo 100 caracteres."),
 
     co_password: yup
     .string()
-    .oneOf([yup.ref('password')], "Senhas devem ser iguais.")
-    .required("Confirmação da senha deve ser preenchida."),
+    .required("Confirmação da senha deve ser preenchida.")
+    .oneOf([yup.ref('password')], "Senhas devem ser iguais."),
 });
 
 export default schema;
