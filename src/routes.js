772dd491d21e0351ea6d3272e768f21311b40849
RcleydsonR <rafael.cleydson@gmail.com>
2021-03-27 18:00:35 -0300

    (#117) Consertando arquivo de rotas e adicionando o PrivateRoutes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,27 @@
 import RegisterStudent from './views/RegisterStudent';
-import Login from './views/Login'
-import RegisterProfessor from './views/RegisterProfessor'
+import Login from './views/Login';
+import RegisterProfessor from './views/RegisterProfessor';
+import Application from './views/Application';
+import ProfessorSearch from './views/ProfessorSearch';
+import isAuthenticated from './services/authentication/index';
 
 import {
     BrowserRouter as Router,
     Switch,
     Route,
+    Redirect
 } from "react-router-dom";
+import LayoutApp from './components/LayoutApp';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={props => (
+        isAuthenticated ? (
+            <Component {...props} />
+        ) : (
+            <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+        )
+    )} />
+);
 
 const Routes = () => (
     <Router>
@@ -16,8 +31,11 @@ const Routes = () => (
             <Route path="/login" component={Login}> <Login /> </Route>
 
             <Route path="/cadastro/professor" component={RegisterProfessor}> <RegisterProfessor /> </Route>
-  
+            <LayoutApp>
+                <PrivateRoute path="/home" component={Application} />
+                <PrivateRoute path="/professor/search/:professorName" component={ProfessorSearch} />
+            </LayoutApp>
         </Switch>
     </Router>
-)
+);
 export default Routes;
