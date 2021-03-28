b47e2da63a766bec003ce2c2124c412bdee283ff
Roberto <robertosantana2001@gmail.com>
2021-03-28 14:18:28 -0300

    (#117) Redirecionando pagina de login para home ao clicar em confirmar

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,19 +1,26 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect} from "react";
 import Button from "../../components/Button";
 import Form from "../../components/Form";
 import Input from "../../components/Input";
 import "./index.css";
 import schema from "./validations"
+import { useHistory } from 'react-router-dom';
 
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
-import isAuthenticated, {sendLogin} from '../../services/authentication'
+import isAuthenticated, {sendLogin, logOut} from '../../services/authentication'
 
 export default function Login() {
+    const history = useHistory();
     const [erroLogin, setErroLogin] = React.useState();
+    
     const { register, handleSubmit, errors } = useForm({
       resolver: yupResolver(schema),
     });
+    
+    useEffect(() => {
+      logOut();
+    })
 
     function onSubmit(data) {
      console.log(data);
@@ -21,9 +28,10 @@ export default function Login() {
      sendLogin(data.email, data.password);
 
      setTimeout(function(){
-      console.log("token = " + localStorage.getItem('access_token'));
       if (!isAuthenticated())
         createSpanError();
+      else 
+        history.push("/home");
      }, 100);
     }
 
@@ -31,13 +39,10 @@ export default function Login() {
       setErroLogin(
        <div className="Erro">
          Email ou Senha Inválidos
-         <Button id="closeSpan" type="button" onClick={fechaErro} text="X" />
+         <Button id="closeSpan" type="button" onClick={() => setErroLogin('')} text="X" />
        </div>)
      }
      
-    function fechaErro(){
-      setErroLogin('');
-    }
     return (
       <div className="Login">
         <header className="Header">
