7bd92598db6c2244045d1ebc6ac7c371ddd93594
Roberto <robertosantana2001@gmail.com>
2021-04-05 21:30:48 -0300

    (#122) Atualizando component do botao para ser StyledComponent

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,12 +1,21 @@
 import React from "react";
-import "./index.css";
+import styled from 'styled-components';
+
+const Btn = styled.button`
+    background: ${props => props.backColor ? props.backColor : "palevioletred"};
+    border: 1px solid rgba(255, 245, 157, 0.6);
+    box-sizing: border-box;
+    border-radius: 10px;
+    padding: ${props => props.padding ? props.padding : '15px 10px'};
+    margin-inline: 15px;
+    box-shadow: 2px 2px grey;`    
 
 export default function Button(props) {
     return (
-        <div className="Button">
-            <button onClick={props.onClick}>
+        <div>
+            <Btn onClick={props.onClick} backColor={props.backColor} padding={props.padding}>
                 {props.text}
-            </button>
+            </Btn>
         </div>
     );
 }
