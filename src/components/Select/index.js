a0d10c0482edcd303db5d924f076b4a66d4d8c20
Victor Hugo <hugo.victorcsi@gmail.com>
2021-04-06 20:42:26 -0300

    (#122) Transformando Select em styled-components

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -1,14 +1,24 @@
 import React from "react";
+import styled from 'styled-components';
 
+const SelectStyle = styled.select`
+    display: inline-block;
+    border: none;
+    border-bottom: 1px solid #000000;
+    background: ${props => props.backColor ? props.backColor : "white"};
+    height: inherit;
+    align-self: right;
+    width: ${props => props.width ? props.width : "inherit"};
+`;
 
-export default function Select({ text, id, name, register, options}){
+export default function Select({ text, id, name, register, options, backColor, width}){
     return(
         <div className="Select">
-            <select id={id} name={name} ref={register}>
+            <SelectStyle id={id} name={name} ref={register} backColor={backColor} width={width}>
                 {options?.map((item) => (
                     <option value={item.id} key={item.id}>{item.name}</option>
                 ))}
-            </select>
+            </SelectStyle>
         </div>
     );
 }
