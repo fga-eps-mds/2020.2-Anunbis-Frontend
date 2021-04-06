493e9f91c8c63f708d742f2de717fb7b41498249
Roberto <robertosantana2001@gmail.com>
2021-04-06 12:59:06 -0300

    (#122) Transformando feed em StyledComponent

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,31 +1,57 @@
 import React from 'react';
-import "./index.css";
+import styled from 'styled-components';
 
 const Title = ({ children }) => {
+  const Title = styled.div`
+    width: inherit;
+    height: 5%;
+    background-color: #212121;
+    color: #FFFDE7;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
+
   return (
-    <div className="Title">
+    <Title>
       {children}
-    </div>
+    </Title>
   )
 }
 
 const Content = ({ children }) => {
+  const Content = styled.div`
+    height: inherit;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    overflow-y: auto;
+`;
+
   return (
-    <div className="Content_Feed">
+    <Content>
       {children}
-    </div>
+    </Content>
   )
 }
 
 export default function Feed({ title, children }) {
+  const Feed = styled.div`
+    width: 450px;
+    height: 600px;
+    border-bottom-right-radius: 2%;
+    border-bottom-left-radius: 2%;
+    background-color: #FFFDE7;
+  `
   return (
-    <div className="Feed">
+    <Feed>
       <Title>
         Resultado da pesquisa "{title}"
       </Title>
       <Content>
         {children}
       </Content>
-    </div>
+    </Feed>
   );
 }
