d601357162a84030da92f2a8ec1c267626519b15
Roberto <robertosantana2001@gmail.com>
2021-04-10 20:13:57 -0300

    (#140) Adicionando componente ResetPassword com index.js e validations.js

---
+++ src/components/ResetPassword/validations.js
@@ -0,0 +1,24 @@
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+
+    old_password: yup
+    .string()
+    .min(8, "Contém no mínimo 8 caracteres.")
+    .required("Senha atual deve ser preenchida."),
+
+    new_password: yup
+    .string()
+    .min(8, "Precisar conter no mínimo 8 caracteres.")
+    .required("Nova senha deve ser preenchida.")
+    .notOneOf([yup.ref('old_password')], "Nova senha deve ser diferente da senha atual."),
+
+    confirm_new_password: yup
+    .string()
+    .min(8, "Precisar conter no mínimo 8 caracteres.")
+    .required("Confirmação da senha deve ser preenchida.")
+    .oneOf([yup.ref('new_password')], "Senhas devem ser iguais."),
+
+})
+
+export default schema;
