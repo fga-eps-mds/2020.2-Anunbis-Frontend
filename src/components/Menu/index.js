988205e9aa82bf4d4b8a258475779a402a06355d
Roberto <robertosantana2001@gmail.com>
2021-04-10 20:18:38 -0300

    (#140) Reajustando styledComponents do Menu e do LayoutApp

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -4,6 +4,8 @@ import Input from '../Input';
 import {useHistory} from "react-router-dom";
 import styled from 'styled-components';
 import Button from '../Button'
+import MenuOptions from '../MenuOptions';
+import {logOut} from '../../services/authentication'
 
 function ProfessorSearch ({history}) {
     const onSubmit = (data) => {
@@ -11,11 +13,26 @@ function ProfessorSearch ({history}) {
             history.push("/professor/search/" + data.target.value.trim())
     }
     return (<ProfessorSearchStyle><Input type="text" width="400px" text="Informe o nome do professor" onkeydown={onSubmit}/></ProfessorSearchStyle>)
-}
+} 
 
 export default function Menu() {
 
     const history = useHistory();
+    const [menuOptions, setMenuOptions] = React.useState("");
+
+    function makeMenuOptions(){
+        if(menuOptions==""){
+            return (setMenuOptions(
+                <MenuOptions>
+                    <Button type='button' backColor='#FFD54F' text='CONFIGURAR' padding='3px' onClick={() => {setMenuOptions(""); history.push('/profile')}}/>
+                    <Button backColor='#FFD54F' text='SOBRE' padding='3px' onClick={() => console.log("sobre")} />
+                    <Button backColor='#FFD54F' text='SAIR' padding='3px' onClick={() => {logOut(); history.push('/user/login')}} />
+                </MenuOptions>
+            ))
+        }else{
+            setMenuOptions("");
+        }
+    }
 
     return (
         <MenuBar>
@@ -23,7 +40,10 @@ export default function Menu() {
                 <ImageLogo src={Name_Logo} alt="logo"></ImageLogo>
             </Logo>
             <ProfessorSearch history={history}/>
-            <BtnEdition text='ººº' padding='5px 5px' backColor='#212121' onClick={() => history.push("/profile")}/>
+            <Container>
+                <BtnEdition text='ººº' padding='5px 5px' backColor='#212121' onClick={() => makeMenuOptions()} />
+                {menuOptions}
+            </Container>
         </MenuBar>
     );
 }
@@ -67,11 +87,23 @@ const ProfessorSearchStyle = styled.div`
 `;
 
 const BtnEdition = styled (Button)`
-    margin-left: auto;
+    margin-top: 10px;
     letter-spacing: 1.5px;
     color: #FFD54F;
     border: none;
     box-shadow: none;
+`
+
+const Container = styled.div`
+    background-color: inherit;
     display: flex;
-    align-self: flex-end;
+    flex-direction: column;
+    align-items: center;
+    width: 100px;
+    height: 2px;
+    margin-left: auto;
+    margin-inline-end: 50px;
+    letter-spacing: 1.5px;
+    color: #FFD54F;
+    box-shadow: none;
 `
