c7ff24e46523646ce8a38727ad1a6f0974543625
oEduardoAfonso <oeduardoafonso@gmail.com>
2021-04-13 17:14:53 -0300

    (#138) Adicionando checkboxes

---
+++ src/components/Checkbox/index.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+const Checkmark = styled.span`
+    height: 20px;
+    width: 20px;
+    margin-right: 10px;
+    border-radius: 5px;
+    background-color: #eee;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Container = styled.label`
+  font-size: 14px;
+  display: flex;
+  align-items: center;
+  width: fit-content;
+  height: fit-content;
+
+  &:hover input ~ ${Checkmark} {
+    background-color: #ccc;
+  }
+
+  input:checked ~ ${Checkmark} {
+    background-color: #26A69A;
+  }
+
+  input:checked ~ ${Checkmark}:after {
+    content: "";
+    left: 10px;
+    top: 5px;
+    width: 3px;
+    height: 10px;
+    border: solid white;
+    border-width: 0 3px 3px 0;
+    -webkit-transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    transform: rotate(45deg);
+
+  }
+
+  input {
+      opacity: 0;
+      cursor: pointer;
+      height: 0;
+      width: 0;
+  }
+`;
+
+
+
+export default function Checkbox({text}){
+    return(
+        <Container>
+            <input type="checkbox"/>
+            <Checkmark/>
+            {text}   
+        </Container>
+    );
+}
--- src/components/Report/index.js
+++ src/components/Report/index.js
@@ -1,17 +1,28 @@
 import styled from 'styled-components';
+import Checkbox from '../Checkbox';
 import FeedPopup from '../FeedPopup';
 
 const Options = styled.div`
+width: 340px;
+margin-top: 10px;
 
-  
+label{
+    margin: 10px;
+}
 `;
+
+
 
 export default function Report(){
     
     return(
         <FeedPopup title="Denuncia">
             <Options>
-                Motivo da Denuncia:
+                Motivo da Denuncia:<br/>
+                <Checkbox text="Linguagem ofensiva"/>
+                <Checkbox text="Comentário preconceituoso"/>
+                <Checkbox text="Críticas não relacionadas a disciplina"/>
+                <Checkbox text="Outros"/>
             </Options>
         </FeedPopup>
     );
