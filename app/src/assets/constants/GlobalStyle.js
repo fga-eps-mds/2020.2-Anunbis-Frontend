6cd53b8f4ad09eb0bdbe0b514488d86e030700de
Eduardo Afonso <oeduardoafonso@gmail.com>
2021-04-16 20:40:29 -0300

    (#170) Adicionando fontes e padroes no Global Style

--- app/src/assets/constants/GlobalStyle.js
+++ app/src/assets/constants/GlobalStyle.js
@@ -1,6 +1,24 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    font-family: -apple-system, 'Roboto', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+html, body, #root {
+    max-height: 100vh;
+    max-width: 100vw;
+
+    height: 100%;
+    width: 100%;
+
+    
+}
     :root{
         --yellow: #FFD54F;
         --black: #212121;
--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,5 @@
 import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import GlobalStyle from './assets/constants/GlobalStyle';
 
 import Routes from './routes/routes';
