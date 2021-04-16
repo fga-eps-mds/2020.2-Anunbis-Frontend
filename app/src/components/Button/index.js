4659bf801a5f91691c0f4d700d3ea827ad0c869c
Eduardo Afonso <oeduardoafonso@gmail.com>
2021-04-16 17:20:52 -0300

    (#170) Adicionando arquivo de styles do Button

--- app/src/components/Button/index.js
+++ app/src/components/Button/index.js
@@ -1,22 +1,6 @@
 import React from "react";
-import styled from 'styled-components';
 
-const Btn = styled.button`
-    background: ${props => props.backColor ? props.backColor : ""};
-    border: 1px solid rgba(255, 245, 157, 0.6);
-    box-sizing: border-box;
-    border-radius: ${props => props.radius ? props.radius : "10px"};
-    padding: ${props => props.padding ? props.padding : '15px 10px'};
-    margin-inline: 15px;
-    
-    &:focus{
-        outline-width: 0;
-    }
-    
-    &:hover{
-        cursor: ${props => props.cursor ? props.cursor : 'pointer'};
-    }
-`  
+import { Btn } from './styles'
 
 export default function Button(props) {
     return (
