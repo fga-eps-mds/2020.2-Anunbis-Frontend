3fdd0c487a4597c4b12e304b36ca8f39db3d8d2a
thiagohdaqwl <thiagohdaqw@gmail.com>
2021-04-27 13:51:36 -0300

    (#180) aumentando o background pra cobrir toda a tela

--- app/src/components/LayoutApp/styles.js
+++ app/src/components/LayoutApp/styles.js
@@ -2,15 +2,20 @@ import styled from 'styled-components';
 import bg_app from "../../assets/images/BG_App.png";
 
 export const Container = styled.div`
-    height: 100vh;
+    width: 100%;
+    min-height: 100%;
     background-image: url(${bg_app});
     display: flex;
     flex-direction: column;
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
 `
 
 export const Main = styled.main`
     width: fit-content;
-    margin-top:20px;
+    margin-top: 20px;
     display: flex;
     align-items: space-around;
     align-self: center;
