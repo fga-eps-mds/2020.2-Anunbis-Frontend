1f0fd5bcffecb8ba536c57f8050ac97fa9f8f21a
thiagohdaqwl <thiagohdaqw@gmail.com>
2021-04-28 19:09:13 -0300

    (#180) adicionando disciplinas dos professores encontrados

--- app/src/views/ProfessorSearch/index.js
+++ app/src/views/ProfessorSearch/index.js
@@ -2,14 +2,18 @@ import React, { useState } from 'react';
 import { useParams } from "react-router-dom";
 import Feed from '../../components/Feed';
 import api from '../../services/Api';
-import { Container } from './styles'
+import { Container, FoundDiv, Img, FoundHeader, Name, Discipline } from './styles'
+import Btn_options from '../../assets/images/Btn_options.png'
 
-export default function ProfessorSearch() {
+function ProfessorSearch() {
     const { professorName } = useParams();
     const [professors, setProfessors] = useState([]);
-    const [indexProfessorSelected, setIndexProfessorSelected] = useState(0);
+    const [professorSelected, setProfessorSelected] = useState(0);
     const [newAvaliationState, setNewAvaliationState] = useState(false);
-    const posts = professors[indexProfessorSelected] ? professors[indexProfessorSelected].posts : []
+    const [disciplineSelected, setDisciplineSelected] = useState(-1)
+    const professor = professors[professorSelected];
+    const posts = getPosts(professor, disciplineSelected)
+    const feedbacks = getFeedbacks(professor, posts, disciplineSelected)
 
     React.useEffect(() => {
         api.get("/professor/" + professorName)
@@ -20,14 +24,64 @@ export default function ProfessorSearch() {
             })
     }, [professorName, newAvaliationState]);
 
+    function onClickProfessor(index) {
+        setProfessorSelected(index)
+        setDisciplineSelected(-1)
+    }
+
     return (<Container>
-        <Feed title="Professores Encontrados" width="210px" radius="0px 0px 10px 10px">
+        <Feed title="Professores" width="210px" radius="0px 0px 10px 10px">
+            {professors.map((prof, index) => <ProfessorFound professor={prof} onClick={() => onClickProfessor(index)} setDisciplineSelected={setDisciplineSelected} key={prof.id_professor} selected={index === professorSelected} />)}
         </Feed>
 
-        <Feed title={professors[indexProfessorSelected] ? `${professors[indexProfessorSelected].name}` : "Sem Resultados"}>
-            <Feed.Header professor={professors[indexProfessorSelected]} canAvaliate={true} onNewAvaliation={() => setNewAvaliationState(!newAvaliationState)} />
+        <Feed title={professors[professorSelected] ? `${professors[professorSelected].name}` : "Sem Resultados"} radius="0px 0px 10px 10px">
+            <Feed.Header professor={professor} feedbacks={feedbacks} canAvaliate={true} onNewAvaliation={() => setNewAvaliationState(!newAvaliationState)} />
             {posts.length > 0 && <><Feed.Title backColor="#26A69A">Avaliações</Feed.Title> <Feed.PostsBox posts={posts} /> </>}
         </Feed>
     </Container>
     );
 }
+
+const ProfessorFound = ({ professor, onClick, selected, setDisciplineSelected }) => {
+    const [showDisciplines, setShowDisciplines] = useState(false);
+
+    function onClickDiscipline(index) {
+        onClick()
+        setDisciplineSelected(index)
+    }
+
+    return (
+        <FoundDiv>
+            <FoundHeader selected={selected}>
+                <Img src={Btn_options} onClick={() => setShowDisciplines(!showDisciplines)} rotate={showDisciplines ? "90deg" : ""} />
+                <Name onClick={onClick}>
+                    {professor.name}
+                </Name>
+            </FoundHeader>
+
+            {showDisciplines && professor.disciplines.map((disci, index) => <Discipline onClick={() => onClickDiscipline(index)} key={disci.discipline_code}>{`[${disci.discipline_code}] ${disci.name}`}</Discipline>)}
+        </FoundDiv>
+    )
+}
+
+function getPosts(professor, disciplineSelected) {
+    if (professor) {
+        return disciplineSelected === -1 ? professor.posts : getPostsByDiscipline(professor, disciplineSelected);
+    } else
+        return [];
+}
+
+function getPostsByDiscipline(professor, disciplineSelected) {
+    const discipline_code = professor.disciplines[disciplineSelected].discipline_code;
+    return professor.posts.filter(post => post.discipline.discipline_code === discipline_code);
+}
+
+function getFeedbacks(professor, posts, disciplineSelected) {
+    if (disciplineSelected === -1) return professor;
+    if (posts.length === 0) return;
+
+    const rating = posts.reduce((accumulator, p) => accumulator + p.rating, 0) / posts.length;
+    return { 'rating': rating }
+}
+
+export default ProfessorSearch;
--- app/src/views/ProfessorSearch/styles.js
+++ app/src/views/ProfessorSearch/styles.js
@@ -5,3 +5,42 @@ export const Container = styled.div`
     display: grid;
     grid-template-columns: 240px 700px;
 `;
+
+export const FoundDiv = styled.div`
+    margin: 10px;
+    background-color: var(--yellow);
+    border-radius: 5px;
+    box-shadow: 1px 1px rgba(0, 0, 0, 50%);
+    padding: 5px;
+
+    * {
+        cursor: pointer;
+    }
+`;
+
+export const FoundHeader = styled.div`
+    display: flex;
+    flex-direction: row;
+    opacity: ${props => props.selected ? "100%" : "60%"};
+    font-weight: ${props => props.selected ? "bold" : "normal"};
+`;
+
+export const Img = styled.img`
+    height: 20px;
+    transition: transform 0.5s ease;
+    transform: ${props => `rotate(${props?.rotate})`};
+`;
+
+export const Name = styled.div`
+    display: inline-block;
+    width: 100%;
+`;
+
+export const Discipline = styled.div`
+    padding: 5px;
+    margin: 5px;
+    background-color: #FFF9C4;
+    border-radius: 10px;
+    box-shadow: 2px 2px rgba(0, 0, 0, 50%);
+    text-align: center;
+`;
