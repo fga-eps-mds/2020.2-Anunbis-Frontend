3fa81f22d563f7ba216be697f7fcc92edbb45034
thiagohdaqwl <thiagohdaqw@gmail.com>
2021-04-29 00:56:14 -0300

    (#180) adicionando componente de loading
    Co-authored-by: Victor Hugo <hugo.victorcsi@gmail.com>

---
+++ app/src/components/Loading/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import {LoadingDiv, Ball} from './styles'
+
+
+export default function Loading() {
+    return (
+        <LoadingDiv>
+            <Ball delay="0" />
+            <Ball delay="0.1s" />
+            <Ball delay="0.2s" />
+        </LoadingDiv>
+    )
+}
---
+++ app/src/components/Loading/styles.js
@@ -0,0 +1,27 @@
+import styled, { keyframes } from 'styled-components';
+
+
+const translateY = keyframes`
+    0% {
+        transform: translateY(0%);
+    }
+    50% {
+        transform: translateY(-150%);
+    }
+`;
+
+export const Ball = styled.span`
+    width: 10px;
+    height: 10px;
+    background-color: var(--darkBlue);
+    border-radius: 50%;
+    animation: ${translateY} 1s ease-in-out infinite;
+    animation-delay: ${props => props.delay};
+`;
+
+export const LoadingDiv = styled.div`
+    width: 50px;
+    height: fit-content;
+    display: grid;
+    grid-template-columns: auto auto auto;
+`;
