4f897e8fb27b648d726b718120080800a37d573a
Victor Hugo <hugo.victorcsi@gmail.com>
2021-05-03 21:13:22 -0300

    (#135) Arrumando de acordo com o objeto Users
    Co-authored-by: Thiago Paiva <thiagohdaqw@gmail.com>

--- app/src/services/Auth.js
+++ app/src/services/Auth.js
@@ -1,20 +1,5 @@
 import api from './Api'
-
-export function isAuthenticated(){
-  return getToken() !== undefined;
-}
-
-export function isProfessor(){
-  if(getToken()){
-    return localStorage.getItem('professor') ? true : false;
-  }
-}
-
-export function isStudent(){
-  if(getToken()){
-    return localStorage.getItem('student') ? true : false;
-  }
-}
+import users from './Users'
 
 export function getToken() {
   return (localStorage.getItem('access_token'));
@@ -29,9 +14,9 @@ export async function sendLogin(email, password, callback, errorCallback) {
   function writeUser(data){
     localStorage.setItem('access_token', data.access_token);
     if (data.user.reg_student)
-      localStorage.setItem('student', JSON.stringify(data.user));
+      localStorage.setItem(users.STUDENT.localStorageName, JSON.stringify(data.user));
     else 
-      localStorage.setItem('professor', JSON.stringify(data.user));
+      localStorage.setItem(users.PROFESSOR.localStorageName, JSON.stringify(data.user));
   }
 
   api.post("/login", body)
@@ -48,6 +33,6 @@ export async function sendLogin(email, password, callback, errorCallback) {
 
 export function logOut() {
   localStorage.removeItem('access_token');
-  localStorage.removeItem('student');
-  localStorage.removeItem('professor');
+  Object.keys(users).forEach((userKey) => {
+    localStorage.removeItem(users[userKey]?.localStorageName)})
 }
