2883b4812bfc53c7787c48c7fc3726a5c0a559ba
Victor Hugo <hugo.victorcsi@gmail.com>
2021-05-05 21:15:49 -0300

    (#136) Adicionando animacoes

--- app/src/views/ProfessorHome/styles.js
+++ app/src/views/ProfessorHome/styles.js
@@ -1,17 +1,39 @@
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
+
+const translate = keyframes`
+    from{
+        opacity: 0.7;
+        transform: translateY(15%);
+    }
+    to{
+        opacity: 1;
+        transform: translateY(0%);
+    }
+`;
 
 export const Container = styled.div`
     border-radius: 20px;
-    background-color: var(--yellow);
+    background: ${props => props.backColor ? props.backColor : ""};
+    min-height: 190px;
     height : fit-content;  
-    box-shadow: 2px 2px 2px rgba(0, 0, 0, 50%);
+    box-shadow: ${props => props.backColor ? "2px 2px 2px rgba(0, 0, 0, 50%)" : 0};
     display: flex;
     align-items: center;
     flex-direction: column;
     margin: 20px;
     padding-bottom: 20px;
     overflow-y: auto;
-`
+`;
+
+export const ContainerPost = styled.div`
+    background-color: #FFF9C4;
+    box-shadow: 2px 2px 2px rgba(0, 0, 0, 50%);
+    margin-top: 20px;
+    border-radius: 15px;
+    height : fit-content;
+    width : 90%;
+    animation: ${translate} 1s ease;
+`;
 
 export const ContainerOptions = styled.div`
     display: grid;
@@ -20,32 +42,20 @@ export const ContainerOptions = styled.div`
     justify-content: space-evenly;
     font-weight: bold;
     padding: 5px;
-
-`
+`;
 
 export const ImageOptions = styled.img`
     height : 45px;
     transition: transform 1s ease;
     transform: ${props => props.rotate ? `rotate(${props.rotate})` : ""};
-    
-
-`
-export const ContainerPost = styled.div`
-    background-color: #FFF9C4;
-    box-shadow: 2px 2px 2px rgba(0, 0, 0, 50%);
-    margin-top: 20px;
-    border-radius: 15px;
-    height : fit-content;
-    width : 90%;
-
-`
+`;
 
 export const ContainerHeader = styled.div`
     display: flex;
     padding-left: 60px;
-`
+`;
 
-export const BtnHomeProfessor = styled.div `
+export const BtnHomeProfessor = styled.div`
     background-color: var(--lightWhite);
     margin: 90px 100px 0 0;
     height: 150px;
@@ -55,18 +65,18 @@ export const BtnHomeProfessor = styled.div `
     flex-direction: column;
     align-items: center;
     justify-content: space-around;
-       
+`;
 
-`
- export const Home = styled.div `
+export const Home = styled.div`
      display: grid;
      grid-template-columns: 300px 800px;
      
- `
+ `;
+
 export const LoadingBox = styled.div`
-width: 100%;
-height: 20vh;
-display: flex;
-justify-content: center;
-align-items: center;
+    width: 100%;
+    height: 20vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
