28602f21ff75181718973bcf92c95d512803e824
RcleydsonR <rafael.cleydson@gmail.com>
2021-05-12 00:55:33 -0300

    (#208) Adicionando verificação de posts para mostrar o gráfico
    Co-authored-by: Roberto Gabriel <robertosantana2001@gmail.com>

--- app/src/views/ProfessorHome/index.js
+++ app/src/views/ProfessorHome/index.js
@@ -15,10 +15,14 @@ import {
 import Button from '../../components/Button';
 import Loading from '../../components/Loading';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { getPosts } from '../../services/Posts';
 =======
 import Graphics from '../../components/Statistics';
 >>>>>>> (#208) Inicializando componente Statistics e biblioteca react-google-chart
+=======
+import Graphic from '../../components/Statistics';
+>>>>>>> (#208) Adicionando verificação de posts para mostrar o gráfico
 
 const DisciplineContent = ({ discipline }) => {
   const [boxPost, setBoxPost] = React.useState(false);
@@ -134,7 +138,7 @@ export default function ProfessorHome() {
           onClick={() => setIsStatistics(false)}
         />
         <Button
-          text="ESTATÍSTICAS"
+          text="ESTATÍSTICA"
           backColor="var(--yellow)"
           padding="15px 40px"
           radius="20px"
@@ -147,10 +151,15 @@ export default function ProfessorHome() {
   return (
     <Home>
       <OptionsProfessorHome />
-      {isStatistics && <Feed title="Estatísticas">
-        <Graphics />
-      </Feed>
-      }
+      {isStatistics && (
+        <Feed title="Estatística">
+          {Object.keys(disciplines).length === 0 && !loading ? (
+            <Container>Sem avaliações ainda</Container>
+          ) : (
+            <Graphic />
+          )}
+        </Feed>
+      )}
       {!isStatistics && (
         <Feed title="Avaliações sobre você">
           {Object.keys(disciplines).length === 0 && !loading ? (
