6b3662f8f66d2185e39de4dad6a0d399630a5b50
RcleydsonR <rafael.cleydson@gmail.com>
2021-05-12 12:58:21 -0300

    (#208) Adicionando select e lógica de ordenações
    Co-authored-by: Victor Hugo <hugo.victorcsi@gmail.com>

--- app/src/components/Statistics/index.js
+++ app/src/components/Statistics/index.js
@@ -2,11 +2,49 @@
 import { useEffect, useState } from 'react';
 import { Chart } from 'react-google-charts';
 import { getPosts } from '../../services/Posts';
+import Select from '../Select';
 import { Container } from './styles';
+
+function getRating(year, posts) {
+  var arrayYear = posts.filter((post) => {
+    if (post.post_date.substring(0, 4) == year) return post;
+  });
+
+  return arrayYear.length > 0
+    ? arrayYear.reduce((accumulator, p) => accumulator + p.rating, 0) /
+    arrayYear.length
+    : 0;
+}
+
+const getYear = (posts) => {
+  const dados = [
+    ['x', 'Nota']
+  ]
+
+  posts
+    ?.map((post) => {
+      return post.post_date.substring(0, 4);
+    })
+    .filter((year, i, post) => {
+      return post.indexOf(year) === i;
+    })
+    .reverse()
+    .map((year) => {
+      dados.push(([year, getRating(year, posts)]));
+    });
+
+  return dados
+
+}
+
+
 
 export default function Graphic() {
   const [posts, setPosts] = useState([]);
-  const data = [['x', 'Nota']];
+  const [data, setData] = useState([
+    ['x', 'Nota']
+  ]);
+
   const options = {
     width: 500,
     height: 350,
@@ -21,35 +59,33 @@ export default function Graphic() {
     legend: { position: 'right' },
   };
 
-  posts
-    ?.map((post) => {
-      return post.post_date.substring(0, 4);
-    })
-    .filter((year, i, post) => {
-      return post.indexOf(year) === i;
-    })
-    .reverse()
-    .map((year) => {
-      data.push([year, getRating(year)]);
-    });
+
 
   useEffect(() => {
     getPosts(setPosts);
   }, []);
 
-  function getRating(year) {
-    var arrayYear = posts.filter((post) => {
-      if (post.post_date.substring(0, 4) == year) return post;
-    });
 
-    return arrayYear.length > 0
-      ? arrayYear.reduce((accumulator, p) => accumulator + p.rating, 0) /
-          arrayYear.length
-      : 0;
-  }
+
+  const dateArray = [{
+    id: 0, name: 'Ultimos 30 dias',  selected: true
+  }, {
+    id: 1, name: 'Ultimos Seis meses'
+  }, {
+    id: 2, name: 'Ultimo ano'
+  }, {
+    id: 3, name: 'Mais antigos', fun: getYear(posts)
+  }];
 
   return (
     <Container>
+      <Select
+        id='datas'
+        backColor="#FFFDE7"
+        text="Selecione uma data"
+        options={dateArray}
+        onChange={(e) => setData(dateArray[e.target.value].fun)}
+        name="id_date" />
       {posts.length > 0 && (
         <Chart chartType="LineChart" data={data} options={options} />
       )}
--- app/src/components/Statistics/styles.js
+++ app/src/components/Statistics/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
