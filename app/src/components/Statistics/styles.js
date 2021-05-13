5183e1ab61af9dc83d2b3479e1a49d11935aab1a
Victor Hugo <hugo.victorcsi@gmail.com>
2021-05-13 00:53:52 -0300

    (#208) Adicionando styles do Loading e a animacao dos graficos
    Co-authored-by: Thiago Paiva <thiagohdaqw@gmail.com>
    Co-authored-by: Rafael Cleydson <rafael.cleydson@gmail.com>

--- app/src/components/Statistics/styles.js
+++ app/src/components/Statistics/styles.js
@@ -1,10 +1,29 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
+const translate = keyframes`
+    from{
+        opacity: 0.7;
+        transform: translateY(15%);
+    }
+    to{
+        opacity: 1;
+        transform: translateY(0%);
+    }
+`;
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
   width: 100%;
   height: 400px;
+  animation: ${translate} 1s ease;
+`;
+
+export const LoadingBox = styled.div`
+  width: 100%;
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
