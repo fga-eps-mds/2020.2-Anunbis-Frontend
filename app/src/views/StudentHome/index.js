06e5a72a248ee54195840fa1c4d8f4694b60ad37
Victor Hugo <hugo.victorcsi@gmail.com>
2021-05-13 12:33:19 -0300

    (#132) Retirando o styles do select em ProfessorSearch e StudentHome

--- app/src/views/StudentHome/index.js
+++ app/src/views/StudentHome/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Feed from '../../components/Feed';
 import { getPosts } from '../../services/Posts';
-import { Conteiner } from './styles';
 
 export default function StudentHome() {
   const [posts, setPosts] = React.useState([]);
@@ -11,10 +10,8 @@ export default function StudentHome() {
   }, []);
 
   return (
-    <Conteiner>
       <Feed title="Avaliações Feitas Por Você">
         <Feed.PostsBox posts={posts} canReport />
       </Feed>
-    </Conteiner>
   );
 }
--- app/src/views/StudentHome/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Conteiner = styled.div`
-  select {
-    margin-top: 5px;
-    margin-right: 5px;
-    margin-left: auto;
-  }
-`;
