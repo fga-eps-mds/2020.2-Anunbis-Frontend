3fdcbe1c084f79f268c52df4cdcf5f8d600679c3
RcleydsonR <rafael.cleydson@gmail.com>
2021-05-14 19:15:55 -0300

    (#205) Adicionando json server e testando services/orders

--- app/src/tests/servicesTests/Order.test.js
+++ app/src/tests/servicesTests/Order.test.js
@@ -1,24 +1,107 @@
 import renderer from 'react-test-renderer';
 import { orderPop, orderDate, orderHRate, orderLRate, orders } from '../../services/Orders';
 
-describe('Snapshot Order service', () => {
-    it('matches the snapshot', () => {
-        const tree = renderer.create(orderPop).toJSON()
-        expect(tree).toMatchSnapshot()
+describe('orderPop service test', () => {
+    function createPostWithAgree(num){
+        const post = {
+            feedbacks: {
+                agrees: num
+            }
+        }
+        return post;
+    }
+    it('must return -1 on orderPop', () => {
+        const post1 = createPostWithAgree(5);
+        const post2 = createPostWithAgree(4);
+        expect(orderPop(post1, post2)).toBe(-1);
     })
-    it('matches the snapshot', () => {
-        const tree = renderer.create(orderDate).toJSON()
-        expect(tree).toMatchSnapshot()
+    it('must return 1 on orderPop', () => {
+        const post1 = createPostWithAgree(4);
+        const post2 = createPostWithAgree(5);
+        expect(orderPop(post1, post2)).toBe(1);
     })
-    it('matches the snapshot', () => {
-        const tree = renderer.create(orderHRate).toJSON()
-        expect(tree).toMatchSnapshot()
+    it('must return 0 on orderPop', () => {
+        const post1 = createPostWithAgree(5);
+        const post2 = createPostWithAgree(5);
+        expect(orderPop(post1, post2)).toBe(0);
     })
-    it('matches the snapshot', () => {
-        const tree = renderer.create(orderLRate).toJSON()
-        expect(tree).toMatchSnapshot()
-    })
-    test('matches the snapshot', () => {
-        expect(orders).toMatchSnapshot();
-    });
 })
+
+describe('orderDate service test', () => {
+    function createPostWithDate(date){
+        const post = {
+            post_date: date
+        }
+        return post;
+    }
+    it('orderDate must return -1', () => {
+        const post1 = createPostWithDate(2021);
+        const post2 = createPostWithDate(2020);
+        expect(orderDate(post1, post2)).toBe(-1);
+    })
+    it('orderDate must return 1', () => {
+        const post1 = createPostWithDate(2020);
+        const post2 = createPostWithDate(2021);
+        expect(orderDate(post1, post2)).toBe(1);
+    })
+    it('orderDate must return 0', () => {
+        const post1 = createPostWithDate(2020);
+        const post2 = createPostWithDate(2020);
+        expect(orderDate(post1, post2)).toBe(0);
+    })
+})
+
+describe('orderHRate service test', () => {
+    function createPostWithRating(rating){
+        const post = {
+            rating: rating
+        }
+        return post;
+    }
+    it('orderDate must return -1', () => {
+        const post1 = createPostWithRating(5);
+        const post2 = createPostWithRating(4);
+        expect(orderHRate(post1, post2)).toBe(-1);
+    })
+    it('orderDate must return 1', () => {
+        const post1 = createPostWithRating(4);
+        const post2 = createPostWithRating(5);
+        expect(orderHRate(post1, post2)).toBe(1);
+    })
+    it('orderDate must return 0', () => {
+        const post1 = createPostWithRating(4);
+        const post2 = createPostWithRating(4);
+        expect(orderHRate(post1, post2)).toBe(0);
+    })
+})
+
+describe('orderLRate service test', () => {
+    function createPostWithRating(rating){
+        const post = {
+            rating: rating
+        }
+        return post;
+    }
+    it('orderDate must return -1', () => {
+        const post1 = createPostWithRating(4);
+        const post2 = createPostWithRating(5);
+        expect(orderLRate(post1, post2)).toBe(-1);
+    })
+    it('orderDate must return 1', () => {
+        const post1 = createPostWithRating(5);
+        const post2 = createPostWithRating(4);
+        expect(orderLRate(post1, post2)).toBe(1);
+    })
+    it('orderDate must return 0', () => {
+        const post1 = createPostWithRating(4);
+        const post2 = createPostWithRating(4);
+        expect(orderLRate(post1, post2)).toBe(0);
+    })
+})
+
+describe('orders service test', () => {
+    it('orders must be the same', () => {
+        expect(orders).toMatchSnapshot();
+    })
+})
+
--- app/src/tests/servicesTests/Posts.test.js
+++ app/src/tests/servicesTests/Posts.test.js
@@ -1,9 +1,28 @@
 import renderer from 'react-test-renderer';
 import { getPosts } from '../../services/Posts';
+import React from 'react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import {createPostHandler, data, setPosts} from '../../mocks/handlers/post-handler';
+
+// const api = setupServer(
+//     rest.get('/post', (req, res, ctx) => {
+//       return res(ctx.json({ posts: 'hello there' }))
+//     })
+//   )
+
+// beforeAll(() => server.listen())
+// afterEach(() => server.resetHandlers())
+// afterAll(() => server.close())
 
 describe('Snapshot Api component', () => {
-    it('matches the snapshot', () => {
-        const tree = renderer.create(getPosts).toJSON() 
-    expect(tree).toMatchSnapshot()
+    it('getting Posts', () => {
+    const data = {};
+    const setPosts = (data) => {
+        data = data
+    }
+
+    getPosts(setPosts)
+    expect(data).toBe();
     })
 })
