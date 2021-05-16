935c52dbdcd7074c0860c16168037f499ff416da
Roberto Mangabeira <robertosantana2001@gmail.com>
2021-05-15 23:31:24 -0300

    (#205) Atualizando snapshot de Avaliation e melhorando teste de auth

--- app/src/tests/servicesTests/Auth.test.js
+++ app/src/tests/servicesTests/Auth.test.js
@@ -4,13 +4,13 @@ describe('Snapshot Auth component', () => {
     test('been login', done => {
         const email = "123456789@aluno.unb.br";
         const senha = "00000000";
-    function callback(data){
-        expect(data).to();
-        done();
-    }
-    function errorCallback(data){
-        callback(data);
-    }
-    sendLogin(email, senha, callback, errorCallback);
+        function callback(data){
+            expect(data).toBeUndefined()
+            done();
+        }
+        function errorCallback(data){
+            callback(data);
+        }
+        sendLogin(email, senha, callback, errorCallback);
     })
 })
