598770eeb536ad34e3afd273eb975000692c2e2f
Roberto Mangabeira <robertosantana2001@gmail.com>
2021-05-15 23:28:15 -0300

    (#205) Melhorando testes de avaliacao e excludeAcc

--- app/src/tests/componentsTests/Avaliation.test.js
+++ app/src/tests/componentsTests/Avaliation.test.js
@@ -1,9 +1,76 @@
 import renderer from 'react-test-renderer';
 import Avaliation from '../../components/Avaliation';
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe('Snapshot Avaliation view', () => {
     it('matches the snapshot', () => {
         const tree = renderer.create(<Avaliation />).toJSON() 
     expect(tree).toMatchSnapshot()
     })
+    describe('test button PUBLICA and ANONIMA', () => {
+        it('children´s of the buttons must be the same on click them', () => {
+            render(<Avaliation />);
+            const publica = { 
+                button: screen.getByText('PÚBLICA'),
+                old_children: screen.getByText('PÚBLICA').children
+            }
+            const anonima = { 
+                button: screen.getByText('ANÔNIMA'),
+                old_children: screen.getByText('ANÔNIMA').children
+            }
+
+            userEvent.click(publica.button);
+            expect(publica.old_children).toBe(publica.button.children);
+
+            userEvent.click(anonima.button);
+            expect(anonima.old_children).toBe(anonima.button.children);
+        })
+    })
+    // describe('test of submit form', () => {
+    //     it('data must be post in api', done => {
+    //         const data = {
+    //             reg_student: "123456789"
+    //         }
+    //         const professor = {
+    //             id_professor: '',
+    //             disciplines: [{
+    //                 discipline_code: 1,
+    //                 name: "teste1"
+    //             },{
+    //                 discipline_code: 2,
+    //                 name: "teste2"
+    //             }]
+    //         }
+    //         const callback = jest.fn();
+    //         localStorage.setItem('student', data);
+    //         render(<Avaliation close={callback} professor={professor}/>);
+    //         const input = screen.getByTestId('form-aval-1');
+
+    //         const select = screen.getByTestId('select-aval-1');
+    //         userEvent.click(select.children[1]);
+
+    //         const star = screen.getByTestId('star-aval-1');
+    //         userEvent.click(star.children[4].children[0])
+            
+    //         const star1 = screen.getByTestId('star-aval-2');
+    //         userEvent.click(star1.children[4].children[0])
+
+    //         const star2 = screen.getByTestId('star-aval-3');
+    //         userEvent.click(star2.children[4].children[0])
+
+    //         const star3 = screen.getByTestId('star-aval-4');
+    //         userEvent.click(star3.children[4].children[0])
+
+    //         const anonima = screen.getByText('ANÔNIMA');
+    //         userEvent.click(anonima);
+
+    //         const descricao = screen.getByTestId('form-aval-2');
+    //         userEvent.type(descricao, "TESTE DE POST");
+
+    //         fireEvent.submit(input);
+      
+    //         expect(callback).toHaveBeenCalled();
+    //     })
+    // })
 })
--- app/src/tests/componentsTests/ExcludeAccount.test.js
+++ app/src/tests/componentsTests/ExcludeAccount.test.js
@@ -1,10 +1,45 @@
 import renderer from 'react-test-renderer';
 import ExcludeAccount from '../../components/ExcludeAccount';
+import { render, screen, cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+afterEach(cleanup);
 
 describe('Snapshot ExcludeAccount component', () => {
     it('matches the snapshot', () => {
-        const tree = renderer.create(<ExcludeAccount />).toJSON()
-    
+        const tree = renderer.create(<ExcludeAccount />).toJSON() 
     expect(tree).toMatchSnapshot()
     })
+    describe('tests of click on buttons', () => {
+        it('should exec callback on click on button VOLTAR', () => {
+            render(<ExcludeAccount close={callback}/>)
+            const button = screen.getByText('VOLTAR');
+            function callback(){
+                button.text = "CLICKED";
+            }
+            userEvent.click(button);
+            expect(button.text).toBe("CLICKED");
+        })
+        it('the children of buttons must be the same on click', ()=> {
+            function callback(){ // CALLBACK NECESSARIO PARA COMPILACAO DO EXCLUDEACCOUNT
+                console.log("CLICKED!");
+            }
+            render(<ExcludeAccount close={callback}/>)
+            const voltar = {
+                button: screen.getByText('VOLTAR'),
+                old_children: screen.getByText('VOLTAR').children
+            }
+            const excluir = {
+                button: screen.getByText('EXCLUIR'),
+                old_children: screen.getByText('EXCLUIR').children
+            }
+
+            userEvent.click(voltar.button);
+            expect(voltar.button.children).toBe(voltar.old_children);
+
+            userEvent.click(excluir.button);
+            expect(excluir.button.children).toBe(excluir.old_children);
+        })
+    })
+    
 })
