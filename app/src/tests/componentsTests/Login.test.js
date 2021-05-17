94ec04d007ae2d586721d62cbaee25bffe7b075d
Roberto Mangabeira <robertosantana2001@gmail.com>
2021-05-16 23:06:12 -0300

    (#205) Adicionando teste assincrono em Login

--- app/src/tests/componentsTests/Login.test.js
+++ app/src/tests/componentsTests/Login.test.js
@@ -1,9 +1,41 @@
 import renderer from 'react-test-renderer';
 import Login from '../../components/Login';
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import '@testing-library/jest-dom';
 
-describe('Snapshot Login component', () => {
-    it('matches the snapshot', () => {
-        const tree = renderer.create(<Login />).toJSON() 
-    expect(tree).toMatchSnapshot()
+jest.useFakeTimers();
+
+const data = {
+    email: '123456789@aluno.unb.br',
+    password: '00000000'
+}
+
+describe('Test Login component', () => {
+    describe('Snapshot test', () => {
+        it('matches the snapshot', () => {
+            const tree = renderer.create(<Login />).toJSON() 
+        expect(tree).toMatchSnapshot()
+        })
+    })
+    describe('Form submit tests', () => {
+        it('Submit email and password fakes must be create div error and close it when click X', async() => {
+            render(<Login />);
+        
+            const inputEmail = screen.getByTestId('input-login-1');
+            userEvent.type(inputEmail, data.email);
+    
+            const inputPassword = screen.getByTestId('input-login-2');
+            userEvent.type(inputPassword, data.password);
+    
+            userEvent.click(screen.getByText('CONFIRMAR'));
+
+            await waitFor(() => {
+                expect(screen.queryByTestId('button-login-1')).not.toBeNull();
+            }).then(() => {
+                userEvent.click(screen.queryByTestId('button-login-1'));
+                expect(screen.queryByTestId('button-login-1')).toBeNull();
+            });
+        })           
     })
 })
