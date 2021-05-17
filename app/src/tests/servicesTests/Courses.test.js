566c45f497411d80d5bd6dd101033d07c5d38df5
RcleydsonR <rafael.cleydson@gmail.com>
2021-05-17 15:01:36 -0300

    (#205) Configurando upload de testes no codecov

--- app/src/tests/servicesTests/Courses.test.js
+++ app/src/tests/servicesTests/Courses.test.js
@@ -3,7 +3,7 @@ import { getCourses } from '../../services/Courses';
 describe('Snapshot Auth component', () => {
   test('must call the callback on getting Courses', (done) => {
     const json = {
-      id: 1,
+      id_course: 1,
       name: 'Engenharia de Software',
     };
     function callback(data) {
